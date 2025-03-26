c1cbbb349ff4c1706e3d18c39d7e12fc25dd6c3a
Artsiom Klimkovich <113889369+ArtsiomKlimkovich@users.noreply.github.com>
2025-03-26 11:03:03 +0100

    Add files via upload

---
+++ motor.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title>Motocykle</title>
+</head>
+<body>
+    <img src="motor.png" alt="" class="motor">
+    <section class="baner">
+        <h1>Motocykle - moja pasja</h1>
+    </section>
+
+    <section class="lewy">
+        <h2>Gdzie pojechać?</h2>
+        <!-- skrypt 1 -->
+        <?php
+
+        ?>
+    </section>
+
+    <section>
+        <section class="prawy">
+            <h2>Co kupić?</h2>
+            <ol>
+                <li>Honda CBR125R</li>
+                <li>Yamaha YBR125</li>
+                <li>Honda VFR800i</li>
+                <li>Honda CBR1100XX</li>
+                <li>BMW R1200GS LC</li>
+            </ol>
+        </section>
+        <section class="prawy">
+            <h2>Statystyki</h2>
+            <p>Wpisanych wycieczek: </p>
+            <!-- skrypt 2 -->
+            <?php
+
+            ?>
+            <p>Użytkowników forum: 200</p>
+            <p>Przesłanych zdjęć: 1300</p>
+        </section>
+    </section>
+
+    <section class="stopka">
+        <p>Stronę wykonał: Artsiom Klimkovich</p>
+    </section>
+</body>
+</html>
